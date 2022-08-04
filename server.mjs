3ad760c2b6360157163418e6173b7db65cdc318a
farya <ftjamal2017@gmail.com>
2022-08-05 04:31:31 +0500

    hello world server completed

--- server.mjs
+++ server.mjs
@@ -1,10 +1,27 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from 'express';
+import cors from 'cors';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+const app = express();
+app.use(cors());
+const port=3000;
+
+
+app.get('/home', (req, res) => {
+  res.send('I am home page!')
 })
+
+app.get('/profile', (req, res) => {
+  res.send('Iam profile page!')
+})
+
+app.get('/weather', (req, res) => {
+  res.send({
+    weather: `sunny`,
+    temperature: `20`,
+    city: `karachi`,
+  })
+}) 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
